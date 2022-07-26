a0da7499b5a10985c38a3e41565c6ad5b7516af4
Nazarov99 <nazarovdadojon99@gmail.com>
2022-07-26 11:48:55 +0500

    finish project

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'asdhasd iasdia asd',
+        productNumber: '874',
+        paymentStatus: 'FDH',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'asdhasd iasdia asd',
+        productNumber: '874',
+        paymentStatus: 'FDH',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'asdhasd iasdia asd',
+        productNumber: '874',
+        paymentStatus: 'FDH',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'asdhasd iasdia asd',
+        productNumber: '874',
+        paymentStatus: 'FDH',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'asdhasd iasdia asd',
+        productNumber: '874',
+        paymentStatus: 'FDH',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'asdhasd iasdia asd',
+        productNumber: '874',
+        paymentStatus: 'FDH',
+        shipping: 'Pending',
+    },
+];
